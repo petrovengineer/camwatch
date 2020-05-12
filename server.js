d4a602718099025f899484a3737fd0a2ce79ed05
Pavel <petrovengineer@gmail.com>
2020-05-12 13:24:58 +0300

    Change hello

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World on Port 3000');
 })
  
 app.listen(3000)
